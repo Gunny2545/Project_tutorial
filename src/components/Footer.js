e4d532c699872768db8717ece1cce1601d83172e
Gunny2545 <118515766+Gunny2545@users.noreply.github.com>
2023-01-09 19:47:16 +0700

    commit-message

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,4 @@
-import { Container, Row, Col } from "react-bootstrap";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import { Container, Row, } from "react-bootstrap";
 
 export const Footer = () => {
   return (
